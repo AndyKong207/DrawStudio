81f2d2f7d5fee3e70c2c0d6979b32643c7134b0d
AndyKong <andy_kong@163.com>
2023-08-06 23:27:45 +0800

    魔力值

--- packages/stablestudio-ui/src/User/Account/index.tsx
+++ packages/stablestudio-ui/src/User/Account/index.tsx
@@ -1,11 +1,12 @@
 import Cookies from "js-cookie";
 import { logout } from "~/api/modules/user";
 import { Router } from "~/Router";
+import { Theme } from "~/Theme";
 
 const Account = () => {
   const navigate = Router.useNavigate();
   const [userInfo, setUserInfo] = useState<any>();
-
+  const [money, setMoney] = useState<string>();
   useEffect(() => {
     setUserInfo(JSON.parse(localStorage.getItem("user") as string));
   }, []);
@@ -17,6 +18,10 @@ const Account = () => {
     if (resp) {
       navigate("/signin");
     }
+  };
+
+  const handleChangeMoney = (value: string) => {
+    setMoney(value);
   };
 
   return (
@@ -79,6 +84,79 @@ const Account = () => {
             >
               Prompt guide
             </a>
+          </div>
+        </div>
+        <div className="flex flex-wrap gap-4">
+          <div className="flex h-fit flex-col rounded border border-zinc-700 bg-zinc-800 p-5">
+            <div className="flex items-start">
+              <h5 className="mb-3.5 text-xl font-semibold text-neutral-900 dark:text-white">
+                魔力值
+              </h5>
+            </div>
+            <div className="flex flex-row items-end justify-between">
+              <span className="text-brand-400 text-3xl font-bold">867.3</span>
+              <div className="opacity-muted flex text-xs tracking-wide">
+                ~4,336 images
+              </div>
+            </div>
+            <hr className="mb-4 mt-3 h-px border-0 bg-neutral-200 dark:bg-white/5"></hr>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-3">
+                <h1>购买魔力值</h1>
+                <div className="flex flex-col gap-2">
+                  <div className="css-1l8dqey col-span-2 flex items-center overflow-hidden rounded bg-neutral-50 text-neutral-900 duration-200 dark:bg-neutral-700 dark:text-white dark:placeholder-neutral-400">
+                    <div className="relative h-full w-full items-center rounded-r-none shadow-none">
+                      <div className="pointer-events-none absolute bottom-0 left-0 top-0 flex items-center justify-center pl-3">
+                        <span className="select-none text-neutral-900 dark:text-neutral-400">
+                          $
+                        </span>
+                      </div>
+                      <Theme.Input
+                        className="dark:placeholder:text-muted-white h-full w-full rounded bg-transparent px-0 py-2 pl-9 text-base text-black shadow-sm focus:border-transparent focus:outline-none dark:border-none dark:bg-transparent dark:text-white"
+                        value={money}
+                        onChange={handleChangeMoney}
+                      />
+                    </div>
+                    <button className="bg-brand-500 shadow-brand-500-md dark:bg-brand-600 dark:hover:bg-brand-500 border-brand-500 dark:border-brand-600 group relative flex h-fit w-fit cursor-pointer select-none flex-row items-center justify-center gap-0 whitespace-nowrap rounded rounded-l-none rounded-r-sm px-3 py-1.5 pr-4 align-middle text-sm font-light text-white opacity-100 shadow-md duration-100 dark:shadow-none">
+                      <div className="-ml-1 mr-1 h-6 w-0 shrink origin-center p-0.5 opacity-75 duration-100 group-hover:opacity-100"></div>
+                      Buy
+                      <div className="absolute bottom-0 right-2 top-0 flex items-center justify-center"></div>
+                    </button>
+                  </div>
+                  <div className="ml-auto flex flex-col items-end">
+                    1,000&nbsp;credits
+                    <div className="opacity-muted flex text-xs tracking-wide">
+                      ~5,000 images
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="h-fit grow overflow-x-auto rounded border border-zinc-700 bg-zinc-800 p-5">
+            <table className="w-full text-left text-sm text-neutral-500 dark:text-neutral-400">
+              <caption className="mb-4 text-left text-lg font-semibold text-neutral-900 dark:text-white">
+                购买记录
+              </caption>
+              <thead className="border-b border-black/10 text-xs uppercase text-neutral-700 dark:border-white/5 dark:text-neutral-400">
+                <tr>
+                  <th scope="col" className=" py-2">
+                    时间
+                  </th>
+                  <th scope="col" className="w-2/6 py-2">
+                    魔力值
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="whitespace-nowrap pt-3">
+                    06/18/23, 07:59:23 PM
+                  </td>
+                  <td className="pt-3">1,000</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
